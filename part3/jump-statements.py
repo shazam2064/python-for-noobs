4fc7e8a4e507dc6a9cffd2c1131482c996e3e575
shazam <gabrielsalomon.980m@gmail.com>
2021-04-12 08:14:06 -0400

    Added more files.

---
+++ part3/jump-statements.py
@@ -0,0 +1,20 @@
+# for i in range(1, 11):
+#     if (i == 5):
+#         break
+#     print(i)
+# print("Omae Wa Mou Shindeiru")
+
+# num = int(input("Enter num: "))
+# for i in range(2, num):
+#     if (num % i == 0):
+#         print("Not prime")
+#         break
+#     else:
+#         print("Oui, prime")
+#         break
+
+n = int(input("Enter n: "))
+for i in range(1, n + 1):
+    if (i % 3 == 0):
+        continue
+    print(i)
