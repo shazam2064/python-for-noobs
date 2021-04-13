1280620635e06ee4cba3121c727aea8d3cb39c7e
shazam <gabrielsalomon.980m@gmail.com>
2021-04-13 08:18:32 -0400

    Added more files

--- part3/jump-statements.py
+++ part3/jump-statements.py
@@ -13,8 +13,15 @@
 #         print("Oui, prime")
 #         break
 
-n = int(input("Enter n: "))
-for i in range(1, n + 1):
-    if (i % 3 == 0):
-        continue
-    print(i)
+# n = int(input("Enter n: "))
+# for i in range(1, n + 1):
+#     if (i % 3 == 0):
+#         continue
+#     print(i)
+
+num1 = 5
+num2 = 10
+if (num1 == 0):
+    pass
+else:
+    print(num2 // num1)
