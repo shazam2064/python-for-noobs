74ab69b074303ba9b66579debf7b8b4e854e9bb7
shazam <gabrielsalomon.980m@gmail.com>
2021-04-15 07:59:59 -0400

    Added more files

--- part4/functions.py
+++ part4/functions.py
@@ -1,6 +1,16 @@
-def printSadOraNoises():
-    print("*Sad Ora Noises*")
+# def printSadOraNoises():
+#     print("*Sad Ora Noises*")
+#
+#
+# for i in range(0, 5):
+#     printSadOraNoises()
+
+def sumOfTwoNums(n1, n2):
+    sum = num1 + num2
+    print("Sum:", sum)
 
 
-for i in range(0, 5):
-    printSadOraNoises()
+num1 = 5
+num2 = 3
+s = sumOfTwoNums(num1, num2)
+print(s)
