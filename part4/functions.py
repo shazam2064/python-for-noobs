dbcb0063fd6436f5a475d962dcf8224cea00d1ac
shazam <gabrielsalomon.980m@gmail.com>
2021-04-20 08:17:02 -0400

    Added more files

--- part4/functions.py
+++ part4/functions.py
@@ -5,12 +5,25 @@
 # for i in range(0, 5):
 #     printSadOraNoises()
 
-def sumOfTwoNums(n1, n2):
-    sum = n1 + n2
-    return sum
+# def sumOfTwoNums(n1, n2):
+#     sum = n1 + n2
+#     return sum
+#
+#
+# num1 = int(input("Enter num1: "))
+# num2 = int(input("Enter num2: "))
+# s = sumOfTwoNums(num1, num2)
+# print(s)
+
+# sumOfTwoNums = lambda n1, n2: n1 + n2
+# print(sumOfTwoNums(2, 5))
+
+def xyz():
+    # global a
+    a = 5
+    print(a)
 
 
-num1 = int(input("Enter num1: "))
-num2 = int(input("Enter num2: "))
-s = sumOfTwoNums(num1, num2)
-print(s)
+a = 7
+xyz()
+print(a)
