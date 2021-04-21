ad3221c9723801918d94a18b512491ddf4c08a3a
shazam <gabrielsalomon.980m@gmail.com>
2021-04-21 08:19:43 -0400

    Added more files

---
+++ part5/strings.py
@@ -0,0 +1,12 @@
+# s = "Hello"
+# print(s)
+# print(s[0])
+# print(s[-2])
+
+s = "*Sad "
+s1 = "Ora"
+s2 = " Noises*"
+s3 = s + s1 + s2  # s3 = "*Sad " + "Ora" + " Noises*"
+s4 = s3 * 3  # 3 * s2
+
+print(s3)
