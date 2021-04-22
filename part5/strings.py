456e51ce32b783f4e1130327e9f4b557586ad621
shazam <gabrielsalomon.980m@gmail.com>
2021-04-22 07:50:54 -0400

    Added more files

--- part5/strings.py
+++ part5/strings.py
@@ -3,10 +3,33 @@
 # print(s[0])
 # print(s[-2])
 
-s = "*Sad "
-s1 = "Ora"
-s2 = " Noises*"
-s3 = s + s1 + s2  # s3 = "*Sad " + "Ora" + " Noises*"
-s4 = s3 * 3  # 3 * s2
+# s = "*Sad "
+# s1 = "Ora"
+# s2 = " Noises*"
+# s3 = s + s1 + s2  # s3 = "*Sad " + "Ora" + " Noises*"
+# s4 = s3 * 3  # 3 * s2
+#
+# print(s3)
 
-print(s3)
+# s = "*Sad Ora Noises*"
+#
+# if "Ora" in s:
+#     print("It's Star Platinum")
+# else:
+#     print("It's not Star Platinum")
+
+# if "Polnareff" < "Iggy":
+#     print("Smaller")
+# else:
+#     print("Bigger")
+
+# if "Jotaro" != "jotaro":
+#     print("yes")
+# else:
+#     print("no")
+
+s = "*Sad Ora Noises*"
+s1 = s[1:4]
+s2 = s[-4:-1]
+print(s1)
+print(s2)
