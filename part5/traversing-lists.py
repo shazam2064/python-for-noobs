338d697abee53119fea0c3683b28f88f65bb433a
shazam <gabrielsalomon.980m@gmail.com>
2021-04-28 08:27:34 -0400

    Added more files

--- part5/traversing-lists.py
+++ part5/traversing-lists.py
@@ -6,6 +6,26 @@
 #     list1.append(val)
 # print(list1)
 
+# n = int(input("Enter n: "))
+# list1 = [int(input("Enter element: ")) for i in range(0, n)]
+# print(list1)
+
+# n = int(input("Enter n: "))
+# list1 = [int(input("Enter element: ")) for i in range(0, n)]
+# print(list1)
+# for ele in list1:
+#     print(ele)
+
+def linesearch(k, lst):
+    for val in lst:
+        if k == val:
+            return True
+    else:
+        return False
+
+
 n = int(input("Enter n: "))
 list1 = [int(input("Enter element: ")) for i in range(0, n)]
-print(list1)
+key = int(input("Enter key: "))
+res = linesearch(key, list1)
+print(res)
